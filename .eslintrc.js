9e97b8093f45616f28fa0786f32c55d616d7416f
LEXAMETA <lexagreeter@gmail.com>
2025-06-23 18:47:22 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
-    root: true,
-    extends: ['universe/native', 'universe/shared/typescript-analysis'],
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx', '*.d.ts', '*.js', '*.jsx'],
-            parserOptions: {
-                project: './tsconfig.json',
-            },
-        },
-    ],
-
-    plugins: ['prettier', 'eslint-plugin-react-compiler', 'internal'],
-    rules: {
-        'internal/enforce-spacing-values': 'error',
-        'react-compiler/react-compiler': 'error',
-        'prettier/prettier': [
-            'error',
-            {},
-            {
-                usePrettierrc: true,
-            },
-        ],
-        radix: 'off',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        'object-shorthand': ['warn', 'consistent'],
+  root: true,
+  extends: ['universe/native', 'universe/shared/typescript-analysis'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts', '*.js', '*.jsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
-}
+  ],
+  plugins: ['prettier', 'eslint-plugin-react-compiler', 'internal', 'import'],
+  rules: {
+    'internal/enforce-spacing-values': 'warn', // Soften to warn
+    'react-compiler/react-compiler': 'warn',
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    radix: 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'object-shorthand': 'off', // Disable shorthand checks
+    'import/order': 'off', // Disable import order checks
+    'no-mixed-spaces-and-tabs': 'off', // Avoid formatting issues
+    'linebreak-style': ['error', 'unix'], // Enforce LF line endings
+  },
+};
