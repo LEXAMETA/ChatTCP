ca57172cc21dcb9a9b9cd454f8429ed0e4cc7a84
LEXAMETA <lexagreeter@gmail.com>
2025-06-23 22:49:01 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,29 @@
 module.exports = {
-  extends: ['eslint:recommended'],
+  extends: ['universe/native', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     'no-undef': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
   },
   env: {
     node: true,
     browser: true,
     jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 };
