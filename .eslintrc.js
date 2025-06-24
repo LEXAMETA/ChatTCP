e1c129dbd30d96e044de24ab699a4409b2f443d8
LEXAMETA <lexagreeter@gmail.com>
2025-06-23 21:42:41 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,13 @@
 module.exports = {
-  root: true,
-  extends: ['universe/native', 'universe/shared/typescript-analysis'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx', '*.d.ts', '*.js', '*.jsx'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-  ],
-  plugins: ['prettier', 'eslint-plugin-react-compiler', 'internal', 'import'],
+  extends: ['plugin:prettier/recommended'],
   rules: {
-    'internal/enforce-spacing-values': 'off',
-    'react-compiler/react-compiler': 'off', // Disable experimental rule
-    'prettier/prettier': 'off',
-    radix: 'off',
-    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'object-shorthand': 'off',
-    'import/order': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    'linebreak-style': ['error', 'unix'],
-    'indent': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'off', // Soften TS
-    'no-console': 'warn', // Allow console for debugging
+    'no-undef': 'off',
+    'no-unused-vars': 'off'
   },
+  env: {
+    node: true,
+    browser: true,
+    jest: true
+  }
 };
